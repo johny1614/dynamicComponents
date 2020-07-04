26d2a1240ece1e1de70217dc97711d2c3cf4c5f4
Michał Lis <michal.lis@softwareplant.com>
2020-07-04 12:06:21 +0200

    Minimal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ComponentFactoryResolver, ViewContainerRef } from '@angular/core';
+import { ContentComponent } from 'src/app/content/content.component';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: 'my-app',
+  template: ``
 })
-export class AppComponent {
-  title = 'dynamicComponentMinimal';
+export class AppComponent implements AfterViewInit {
+
+  constructor(private componentFactoryResolver: ComponentFactoryResolver,
+              private viewContainerRef: ViewContainerRef) {
+  }
+
+  ngAfterViewInit() {
+    let componentFactory = this.componentFactoryResolver.resolveComponentFactory(ContentComponent);
+    this.viewContainerRef.createComponent(componentFactory);
+  }
+
 }
---
+++ src/app/content/content.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  template: `Hello from content Component :)`,
+})
+export class ContentComponent {
+
+}
